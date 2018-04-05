03bf3d8c7e2ecde5dd4b8ecd680e9ed49947f5a8
benkobalog <benko.balog@gmail.com>
2018-04-05 15:27:17 +0200

    comment

--- app/repository/postgres/PomodoroPqslRepo.scala
+++ app/repository/postgres/PomodoroPqslRepo.scala
@@ -54,7 +54,7 @@ class PomodoroPqslRepo(implicit db: DatabaseDef, ec: ExecutionContext)
           .filter(_.usersId === usersId)
           .filter(_.finished.isEmpty)
           .map(r => (r.started, ts))
-          .take(2) // don't get more than 2 in any case, because we don't need more
+          .take(2) // Normally there should only be one active pomodoro, we take 2 to detect errors
           .result
           .map {
             case Nil => Idle

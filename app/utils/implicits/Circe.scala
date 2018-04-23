e7b69e820bf0315671b6d5aac35fb6e920b38555
benkobalog <benko.balog@gmail.com>
2018-04-23 16:36:29 +0200

    Added createEncoder helper

--- app/utils/implicits/Circe.scala
+++ app/utils/implicits/Circe.scala
@@ -6,21 +6,24 @@ import java.time.Duration
 import io.circe.{Encoder, Json}
 
 object Circe {
+
+  def createEncoder[A](fn: A => Json): Encoder[A] = new Encoder[A] {
+    override def apply(a: A): Json = fn(a)
+  }
+
   implicit val timeStampEncoder: Encoder[java.sql.Timestamp] =
-    new Encoder[java.sql.Timestamp] {
-      override def apply(t: Timestamp): Json =
-        Json.fromString(t.toString.takeWhile(_ != '.'))
-    }
+    createEncoder(
+      timestamp => Json.fromString(timestamp.toString.takeWhile(_ != '.')))
 
   implicit val javaDurationEncoder: Encoder[Duration] =
-    new Encoder[Duration] {
-      override def apply(duration: Duration): Json = {
-        val times = duration.toString.split("-").groupBy(_.last).mapValues(_.mkString)
-        val h = times.get('H').map(_.init + "h")
-        val m = times.get('M').map(_.init + "m")
-        val s = times.get('S').map(s => (s.init.toDouble + 0.5).toInt + "s")
-        val string = List(h, m, s).foldLeft("")((str, t) => str + t.fold("")(_ + " "))
-        Json.fromString(string)
-      }
+    createEncoder { duration =>
+      val times =
+        duration.toString.split("-").groupBy(_.last).mapValues(_.mkString)
+      val h = times.get('H').map(_.init + "h")
+      val m = times.get('M').map(_.init + "m")
+      val s = times.get('S').map(s => (s.init.toDouble + 0.5).toInt + "s")
+      val string =
+        List(h, m, s).foldLeft("")((str, t) => str + t.fold("")(_ + " "))
+      Json.fromString(string)
     }
 }

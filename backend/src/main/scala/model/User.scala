3aef22e546eb33c727c299655a9355fdb07b6b86
benkobalog <benko.balog@gmail.com>
2018-08-27 17:49:13 +0200

    try bootstrap from downloaded source instead of CDN

--- backend/src/main/scala/model/User.scala
+++ backend/src/main/scala/model/User.scala
@@ -4,4 +4,9 @@ case class User(id: java.util.UUID,
                 email: String,
                 createdAt: java.sql.Timestamp,
                 pomodoroSeconds: Int,
-                breakSeconds: Int)
+                breakSeconds: Int
+                // autoStartBreak: Boolean,
+                // stopAtTimeLimit: Boolean,
+                // warnPeriodSeconds: Int,
+                // warnTimes: Int
+               )
--- frontend/typescript/webpack.config.js
+++ frontend/typescript/webpack.config.js
@@ -1,17 +1,17 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
- entry: './src/main.ts',
- resolve: {
-   extensions: ['.webpack.js', '.web.js', '.ts', '.js']
- },
- module: {
-   rules: [
-     { test: /\.ts$/, loader: 'ts-loader' }
-   ]
- },
- output: {
-   filename: 'main.js',
-   path: path.resolve(__dirname+"/../public", 'javascripts')
- }
+  entry: './src/main.ts',
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname+"/../public", 'javascripts')
+  }
 }
